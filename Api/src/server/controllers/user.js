321b0cbb161ba60206fbab395ae4b88f92fe695e
Pooja-22 <pooja.garg@tothenew.com>
2017-10-07 01:07:54 +0530

    Removed dist from gitignore

--- Api/src/server/controllers/user.js
+++ Api/src/server/controllers/user.js
@@ -1,7 +1,12 @@
 import User from '../models/user';
 
 const me = (req, res) => {
-    console.log("inside me")
+    const userId = req.user.id;
+    User.findById(userId, (err, user) => {
+        if (err) res.status(401).json(err);
+        if (!user) return res.status(401).send("Unauthorized");
+        res.json(user);
+    })
 };
 
 module.exports = {
