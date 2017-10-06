aba54508a4777af8f2c2c86862352299726a8dd6
Pooja-22 <pooja.garg@tothenew.com>
2017-10-07 01:17:56 +0530

    user api

--- Api/src/server/controllers/user.js
+++ Api/src/server/controllers/user.js
@@ -1,14 +1,34 @@
 import User from '../models/user';
 
+/**
+ * Get logged in user details
+ * @param req
+ * @param res
+ */
 const me = (req, res) => {
-    const userId = req.user.id;
-    User.findById(userId, (err, user) => {
+    const emailId = req.params.emailId;
+    User.findOne({ emailId }, (err, user) => {
         if (err) res.status(401).json(err);
-        if (!user) return res.status(401).send("Unauthorized");
+        if (!user) return res.status(404).send("Not found");
         res.json(user);
     })
 };
 
+/**
+ * Add user
+ * @param req
+ * @param res
+ */
+const addUser = (req, res) => {
+  User.create(req.body, (err, user) => {
+      if (err) res.status(401).json(err);
+      return res.status(201).json(user);
+  });
+};
+
+
+
 module.exports = {
-  me  
+  me,
+  addUser
 };
