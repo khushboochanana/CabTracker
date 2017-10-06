fc1ac1f13fcad6521b5804ef77f6cd5b46a8754d
Pooja-22 <pooja.garg@tothenew.com>
2017-10-07 02:39:22 +0530

    Cab apis

--- Api/src/server/controllers/cab.js
+++ Api/src/server/controllers/cab.js
@@ -1,8 +1,13 @@
 import Cab from '../models/cab';
 import User from '../models/User';
 
+/**
+ * Fetch cab details
+ * @param req
+ * @param res
+ */
 const cabDetails = (req, res) => {
-  const cabId = req.params.cabId;
+  const cabId = req.params.id;
   Cab.findById({ _id: cabId }, (err, cab) => {
     if (err) res.status(401).json(err);
     if (!cab) return res.status(404).send("Not found");
@@ -10,14 +15,36 @@ const cabDetails = (req, res) => {
   });  
 };
 
+/**
+ * Add cab roster
+ * @param req
+ * @param res
+ */
 const addRoster = (req, res) => {
-    Cab.create(req.body, (err, cab) => {
-        if (err) res.status(401).json(err);
-        let ids = [];
-        cab.cabMates.forEach(cabMate => { ids.push(cabMate._id) });
-        User.update({ _id: { $in:ids } },{ $set: { cabId: cab._id } });
-        return res.status(201).json(user);
+  Cab.create(req.body, (err, cab) => {
+    if (err) res.status(401).json(err);
+    let ids = [];
+    cab.cabMates.forEach(cabMate => { ids.push(cabMate._id) });
+    User.update({ _id: { $in:ids } },{ $set: { cabId: cab._id } });
+      return res.status(201).json(user);
     });
+};
+
+/**
+ * Update cab roster
+ * @param req
+ * @param res
+ */
+const updateRoster = (req, res) => {
+  const { cabId } = req.params;
+  const { userId, presence } = req.query;  
+  if (presence && userId) {
+    Cab.findById({ _id: cabId }, (err, cab) => {
+      if (err) res.status(401).json(err);
+      if (!cab) return res.status(404).send("Not found");
+      
+    })
+  }
 };
 
 module.exports = {
