823caf5e26fc896ead31ed6e11457f06a1e6a2b4
Khushboo Chanana <khushboo.chanana@tothenew.com>
2017-10-08 00:34:22 +0530

    Update Main screen

--- App/src/containers/SplashScreen.js
+++ App/src/containers/SplashScreen.js
@@ -24,41 +24,44 @@ export default class SplashScreen extends Component {
   componentWillMount() {
     setTimeout(() => {
       this.setTimePassed();
-    },15000);
+    },150000);
   }
 
   render() {
     return (
+        <Image style={styles.backgroundImage} source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507401707/ThinkstockPhotos-dv1954035_mfqebj.jpg'}}>
       <View style={styles.container}>
-        <Image style={styles.backgroundImage} source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507400562/sampleMap_ccu0rv.jpg'}}>
-          <Text style={styles.title} >HAcker-Cab-TracKer</Text>
+          <Text style={{position:'absolute',left:100,top:200,fontSize: 20, alignItems: "center",
+            justifyContent: "center" }} >HAcker-Cab-TracKer</Text>
           <Image
-                  style={{position:'absolute',width:150,height:150,left:50}}
+                  style={{position:'absolute',width:150,height:150,left:110,top:120}}
                   source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507387195/cab_snwgzs.png'}}
           />
+      </View>
         </Image>
 
-      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: 'black',
+    opacity: 0.9
   },
   backgroundImage: {
     flex: 1,
-    alignSelf: 'stretch',
-    width: null,
-    opacity:.8,
-    backgroundColor: 'transparent'
+    height: window.height/3.2,
+    width: window.width,
+    flexDirection: 'column'
 
   },
   title:{
     flex: 1,
+    position:'absolute',
+    top:100,
     fontSize: 20,
-    marginTop: 110,
     padding: 60,
     width:500,
     alignItems: "center",
