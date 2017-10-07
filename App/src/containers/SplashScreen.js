91f8484631583880b6a74b1930b37480baf42a03
Priya Choudhary <priya@tothenew.com>
2017-10-08 00:52:42 +0530

    splash screen UI fix

--- App/src/containers/SplashScreen.js
+++ App/src/containers/SplashScreen.js
@@ -4,42 +4,43 @@ import {
   Text,
   StyleSheet,
   AsyncStorage,
-        Image
+  Image
 } from "react-native"
 
 export default class SplashScreen extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   setTimePassed = async () => {
-    let loggedIn = await AsyncStorage.getItem("auth-key")
+    let loggedIn = await AsyncStorage.getItem("auth-key");
     if (loggedIn) {
       this.props.navigation.navigate("List");
     } else {
       this.props.navigation.navigate("LoginScreen");
     }
-  }
+  };
 
   componentWillMount() {
     setTimeout(() => {
       this.setTimePassed();
-    },150000);
+    },3000);
   }
 
   render() {
     return (
-        <Image style={styles.backgroundImage} source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507401707/ThinkstockPhotos-dv1954035_mfqebj.jpg'}}>
-      <View style={styles.container}>
-          <Text style={{position:'absolute',left:100,top:200,fontSize: 20, alignItems: "center",
-            justifyContent: "center" }} >HAcker-Cab-TracKer</Text>
+      <Image
+        style={styles.backgroundImage}
+        source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507401707/ThinkstockPhotos-dv1954035_mfqebj.jpg'}}
+      >
+        <View style={styles.container}>
+          <Text
+            style={{position:'absolute',left:70,top:280,fontSize: 27, alignItems: "center",
+            justifyContent: "center",zIndex: 9999, color: '#fff' }} >HAcker-Cab-TracKer
+          </Text>
           <Image
-                  style={{position:'absolute',width:150,height:150,left:110,top:120}}
-                  source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507387195/cab_snwgzs.png'}}
+            style={{position:'absolute',width:150,height:150,left:110,top:120}}
+            source={{uri: 'http://res.cloudinary.com/hiuj1tri8/image/upload/v1507387195/cab_snwgzs.png'}}
           />
-      </View>
-        </Image>
-
+        </View>
+      </Image>
     )
   }
 }
@@ -47,15 +48,14 @@ export default class SplashScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    opacity: 0.9
+    opacity: 0.8,
+    backgroundColor: 'black'
   },
   backgroundImage: {
     flex: 1,
-    height: window.height/3.2,
+    height: window.height,
     width: window.width,
     flexDirection: 'column'
-
   },
   title:{
     flex: 1,
@@ -66,10 +66,9 @@ const styles = StyleSheet.create({
     width:500,
     alignItems: "center",
     justifyContent: "center"
-
   },
   icon:{
     flex: 1
-    }
+  }
 })
 
